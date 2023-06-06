a0b355c12abbbab3455667093d45af5217af37f5
RevadiSundaram <sundaramrevadi@gmail.com>
2023-06-06 19:26:57 +0530

    Initial Commit

---
+++ src/Content.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ItemsList from "./ItemsList";
+
+function Content({ items, handleCheck, handleDelete }) {
+  return (
+    <main>
+      {items.length ? (
+        <ItemsList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <p
+          style={{
+            marginTop: "2rem",
+          }}
+        >
+          Your list is empty
+        </p>
+      )}
+    </main>
+  );
+}
+export default Content;
